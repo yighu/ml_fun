b442d8f29dd80e219f1fd701384a1eeb99aad439
Yiguang Hu <yiguanghu@Yiguangs-MacBook-Pro-2.local>
2021-12-05 21:22:25 -0500

    start

---
+++ README.cmd
@@ -0,0 +1,19 @@
+#Follow instruction setup docker
+If you already have Docker setup, skip this step
+
+https://docs.docker.com/get-docker/
+
+# Set up
+Create an environment file `notebooks/.env` with any environment variable you would need
+
+https://jupyter-docker-stacks.readthedocs.io/en/latest/
+
+# Running
+Start the project with `docker-compose up`
+
+# verifying
+Make sure docker is running. if not, find the docker app and start it first
+look for docker command instructions like:
+docker ps
+docker images
+
